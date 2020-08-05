d9275ebf87b34a218594e1d4c3914223f993f419
pengchujin <2093959501@qq.com>
2020-08-05 08:47:34 +0800

    add EDNS

--- main.go
+++ main.go
@@ -35,6 +35,22 @@ func (this *dnsServer) ServeDNS (w dns.ResponseWriter, r *dns.Msg) {
 		copy(realIP, addr.IP)
 	}
 	m := dns.Msg{}
+
+	o := new(dns.OPT)
+	o.Hdr.Name = "."
+	o.Hdr.Rrtype = dns.TypeOPT
+	e := new(dns.EDNS0_SUBNET)
+	e.Code = dns.EDNS0SUBNET
+	e.Family = 1	// 1 for IPv4 source address, 2 for IPv6
+	e.SourceNetmask = 32	// 32 for IPV4, 128 for IPv6
+	e.SourceScope = 0
+	e.Address = realIP	// for IPv4
+
+	r.Extra = append(r.Extra, o)
+
+	// e.Address = net.ParseIP("2001:7b8:32a::2")	// for IPV6
+	o.Option = append(o.Option, e)
+
 	m.SetReply(r)
 	log.Println(realIP)
 	log.Println(r)

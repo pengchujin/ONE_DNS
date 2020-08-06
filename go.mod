17c676960cc64418f9dc7381d53a298de13ae913
pengchujin <pengchujin@blackbeanssoft.com>
2020-08-06 16:52:52 +0800

    add redis cache

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module mods
 
 go 1.14
 
-require github.com/miekg/dns v1.1.31
+require (
+	github.com/go-redis/redis/v8 v8.0.0-beta.7
+	github.com/miekg/dns v1.1.31
+)
--- main.go
+++ main.go
@@ -4,7 +4,12 @@ import (
 	"log"
 	"github.com/miekg/dns"
 	"net"
+	"context"
+	"github.com/go-redis/redis/v8"
+	"encoding/json"
 )
+
+var ctx = context.Background()
 
 var records = map[string]string{
 	"test.service.": "192.168.0.2",
@@ -26,6 +31,14 @@ type dnsServer struct {
 }
 
 func (this *dnsServer) ServeDNS (w dns.ResponseWriter, r *dns.Msg) {
+
+	opt, err := redis.ParseURL("redis://localhost:6379/0")
+	if err != nil {
+			panic(err)
+	}
+	rdb := redis.NewClient(opt)
+
+	//获取客户端IP
 	var realIP net.IP
 	if addr, ok := w.RemoteAddr().(*net.UDPAddr); ok {
 		realIP = make(net.IP, len(addr.IP))
@@ -34,31 +47,87 @@ func (this *dnsServer) ServeDNS (w dns.ResponseWriter, r *dns.Msg) {
 		realIP = make(net.IP, len(addr.IP))
 		copy(realIP, addr.IP)
 	}
+	log.Println(realIP)
 	m := dns.Msg{}
 	m.SetReply(r)
-	log.Println(realIP)
-	log.Println(r)
-	switch r.Question[0].Qtype {
-	case dns.TypeA:
-		domain := m.Question[0].Name
-		address, ok := records[domain]
-		if ok {
-			m.Authoritative = true
-			m.Answer = append(m.Answer, &dns.A{
-				Hdr: dns.RR_Header{ Name: domain, Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: 60},
-				A: net.ParseIP(address),
-			})
-			w.WriteMsg(&m)
-		} else {
+
+	domain := m.Question[0].Name
+
+	val, err := rdb.Get(ctx, domain).Result()
+
+	log.Println("++++++++++++ ",r.Question[0].Qtype, " ++++++++++++")
+
+	if err != nil {
+		switch r.Question[0].Qtype {
+		case dns.TypeA:
+			domain := m.Question[0].Name
+			address, ok := records[domain]
+			if ok {
+				m.Authoritative = true
+				m.Answer = append(m.Answer, &dns.A{
+					Hdr: dns.RR_Header{ Name: domain, Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: 60},
+					A: net.ParseIP(address),
+				})
+				w.WriteMsg(&m)
+			} else {
+				// c := dns.Client{Net: "udp", ReadTimeout: 2*time.Second, WriteTimeout: 2*time.Second,}
+				c := new(dns.Client)
+				res, _, err := c.Exchange(r, "8.8.8.8:53")
+				
+				if err != nil {
+					log.Println(err, res)
+				}
+				
+				answerString, err := json.Marshal(res.Answer)
+				if err != nil {
+					log.Println(nil)
+				}
+	
+				error := rdb.Set(ctx, m.Question[0].Name, string(answerString), 0).Err()
+	
+				if(err != nil) {
+					panic(error)
+				}
+	
+				w.WriteMsg(res)
+			}
+		
+		default:
 			// c := dns.Client{Net: "udp", ReadTimeout: 2*time.Second, WriteTimeout: 2*time.Second,}
 			c := new(dns.Client)
 			res, _, err := c.Exchange(r, "8.8.8.8:53")
-			log.Println(res)
 			if err != nil {
 				log.Println(err, res)
 			}
+	
+			answerString, err := json.Marshal(res.Answer)
+				if err != nil {
+					log.Println(nil)
+				}
+	
+			error := rdb.Set(ctx, m.Question[0].Name, string(answerString), 0).Err()
+	
+			if(err != nil) {
+				panic(error)
+			}
+	
 			w.WriteMsg(res)
 		}
-	}
+	} 
 	
+	m.Authoritative = true
+	dnsA := []dns.A{}
+	err = json.Unmarshal([]byte(val), &dnsA)
+	log.Println(dnsA)
+
+	for _, value := range dnsA {
+		m.Answer = append(m.Answer, &value)
+	}
+
+	if err != nil {
+		log.Println(err)
+	}
+	m.Authoritative = true
+	w.WriteMsg(&m)
+
 }
